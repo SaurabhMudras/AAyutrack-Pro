505aae5a02e233e7f25175019f394703c0187244
Saurabh Mudras <mudrassaurabh@gmail.com>
2025-11-12 19:50:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9135722525-8ebdf",
+  "appId": "1:68833685865:web:f027e8693c26f904678df8",
+  "apiKey": "AIzaSyA7vCZxVTMcNxBpkWSvewPtXD8iOwUeI3c",
+  "authDomain": "studio-9135722525-8ebdf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "68833685865"
+};
